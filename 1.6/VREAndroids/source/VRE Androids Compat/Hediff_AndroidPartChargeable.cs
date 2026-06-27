acff6e52f25121fc50f0805446283095a8e43f07
Ahmed Hanie <flubzd@gmail.com>
2026-06-27 11:20:09 +0300

    feat: drop archotech patching and add optional VRE Androids support

---
+++ 1.6/VREAndroids/source/VRE Androids Compat/Hediff_AndroidPartChargeable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRE_Androids_Compat
+{
+    public class Hediff_AndroidPartChargeable : VREAndroids.Hediff_AndroidPart
+    {
+        public override bool Visible => true;
+    }
+}
---
+++ 1.6/VREAndroids/source/VRE Androids Compat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VRE Androids Compat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VRE Androids Compat")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fc2d293b-92af-4d2a-8897-6de7a8c8bf91")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
